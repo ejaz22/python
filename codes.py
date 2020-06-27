b2a16dcbbcb07f3b7b1553b835f953b978e1aea8
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-06-27 13:35:23 +0530

    Delete codes.py

--- codes.py
+++
@@ -1,8 +0,0 @@
-# infer datetime
-import pandas as pd
-df = pd.read_csv('20180112-2.csv')
-df['Month/Day'] = pd.to_datetime(df['Month/Day'], format = '%m/%d')
-print(df)
-
-#
-df.columns.to_list() # or [*df]
